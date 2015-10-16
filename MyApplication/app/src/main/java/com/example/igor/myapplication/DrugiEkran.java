7ebffe5c5b71171e39e4c29e76256eebd73aee7f
icoklo <icoklo@foi.hr>
2015-10-16 17:30:04 +0200

    Složeno pokretanje druge aktivnosti
    Dodan je gumb na glavnu aktivnost, kad se klikne na taj gumb aplikacija
    premješta korisnika na drugu aktivnost.

---
+++ MyApplication/app/src/main/java/com/example/igor/myapplication/DrugiEkran.java
@@ -0,0 +1,13 @@
+package com.example.igor.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DrugiEkran extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_drugi_ekran);
+    }
+}
--- MyApplication/app/src/main/java/com/example/igor/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/igor/myapplication/MainActivity.java
@@ -1,13 +1,33 @@
 package com.example.igor.myapplication;
 
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    public Button gumb;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        onClickButtonListener();
+    }
+
+    public void onClickButtonListener(){
+
+        gumb=(Button)findViewById(R.id.button);
+        gumb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent("com.example.igor.myapplication.DrugiEkran");
+                startActivity(intent);
+            }
+        });
+
     }
 }
