083941804385a2a80812d73e331ccb28fa2cf526
icoklo <icoklo@foi.hr>
2015-11-03 16:49:02 +0100

    testni commit

--- MyApplication/app/src/main/java/com/example/igor/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/igor/myapplication/MainActivity.java
@@ -10,14 +10,31 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
-    public Button gumb;
+    Button gumb;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         onClickButtonListener();
-        Log.d("oznaka","moja poruka");
+
+        Test test;
+        for(int i=1;i<=5;i++) {
+            test = new Test("ime " + i, "prezime " + i, 12);
+            test.lista.add(test);
+            // Log.d("oznakaFor", "velicina:" + test.lista.size());
+        }
+
+        Test pomoc=new Test();
+        int velicina=pomoc.lista.size();
+        Log.d("oznaka1","velicina:"+velicina);
+        // ispis sloziti jos
+
+        // foreach petlja
+        for(Test t:pomoc.lista){
+            Log.d("ispis","korisnik:"+t.lista.indexOf(t)+" "+t.getIme()+t.getPrezime());
+
+        }
+
     }
 
     public void onClickButtonListener(){
@@ -32,6 +49,9 @@ public class MainActivity extends AppCompatActivity {
         });
 
     }
+
+
+
 
 
 }
